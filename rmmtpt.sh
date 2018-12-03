f222ec0d72668107b36829b382bc1c60158c0f3b
Simon Zorin <szorin@ebsco.com>
2018-12-03 15:48:08 -0500

    Added updated file so it can take a list of shares and deleted the old one which was specific to a user story

---
+++ rmmtpt.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+E_BADARGS=85
+
+if [ ! -n "$1" ]
+then
+  echo "Usage: `basename $0` argument1 argument2 etc."
+  exit $E_BADARGS
+fi  
+
+echo
+
+index=1          # Reset count.
+
+echo "Listing mount points to unmount and corresponding directories to delete:"
+for arg in "$@"
+do
+  echo "Arg #$index = $arg"
+  let "index+=1"
+  sed -i -e "/\/${arg}/d" ~/Documents/Scripts/fstabtest
+  umount $arg
+done            
+
+exit 0
