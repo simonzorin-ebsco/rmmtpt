10be8c4653bf8b4c705b487691bc65ace422c795
Unknown <simon.zorin13@gmail.com>
2018-12-03 17:47:08 -0500

    rename some variables for code readability

--- rmmtpt.sh
+++ rmmtpt.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#Version .6
 
 set -o errexit
 set -o pipefail
@@ -7,22 +8,21 @@ E_BADARGS=85
 
 if [ ! -n "$1" ]
 then
-  echo "Usage: `basename $0` argument1 argument2 etc."
+  echo "Usage: `basename $0` filepath1 filepath2 etc."
   exit $E_BADARGS
-fi  
+fi
 
 echo
 
 index=1          # Reset count.
 
 echo "Listing mount points to unmount and corresponding directories to delete:"
-for arg in "$@"
+for filepath in "$@"
 do
-  echo "Arg #$index = $arg"
+  echo "Arg #$index = $filepath"
   let "index+=1"
-  cp /etc/fstab /etc/fstab.bak
-  sed -i -e "/\/${arg}/d" /etc/fstab || true
-  umount $arg || true
-done            
+  sed -i -e "/\/${filepath}/d" /etc/fstab || true
+  umount $filepath || true
+done
 
 exit 0
